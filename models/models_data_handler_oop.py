1e0f24bfb7def1c2342c3bdde06005af4f46bbd6
Gus1101 <augustosantosb60@gmail.com>
2025-05-26 19:04:30 -0300

    build: criando models para validação de schema na transformação de dados

---
+++ models/models_data_handler_oop.py
@@ -0,0 +1,21 @@
+# Libs import
+import pandera.pandas as pa
+from pandera.typing import Series
+
+# Models
+class SchemaExtractExcelDataOut(pa.DataFrameModel):
+    customer_name: Series[str]
+    customer_email: Series[str]
+    customer_age: Series[int] = pa.Field(ge=1, le=100)
+
+
+
+class SchemaTransformDataIn(SchemaExtractExcelDataOut):
+    pass
+
+class SchemaTransformDataOut(pa.DataFrameModel):
+    customer_name: Series[str]
+    customer_email: Series[str]
+
+class SchemaLoadDataIn(SchemaTransformDataOut):
+    pass
