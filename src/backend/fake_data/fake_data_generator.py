e78c5406fa03059e1da4149e702ffaba16a5fbbc
Gus1101 <augustosantosb60@gmail.com>
2025-05-26 17:09:59 -0300

    feat: criada função para geração de dados falsos

---
+++ src/backend/fake_data/fake_data_generator.py
@@ -0,0 +1,30 @@
+#Libs import
+from faker import Faker
+from typing import List, Dict, Any
+from random import randint
+
+#Settings
+fake = Faker("pd-BR")
+
+#Code
+def fake_data_generator(quantity_registers: int) -> List[Dict[str, Any]]:
+
+    """
+    Função utilizada para gerar gama de dados falsos.
+
+    :param quantity_registers: Recebe um número inteiro indicando a quantidade de registros a \
+    serem gerados
+    :return: Retorna uma lista de dicionários contendo os registros gerados.
+    """
+
+    data = []
+
+    for _ in range(quantity_registers):
+        efem = {
+            "customer_name":fake.name(),
+            "customer_email":fake.email(),
+            "customer_age":randint(1,100),
+        }
+        data.append(efem)
+
+    return data
