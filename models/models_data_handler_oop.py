e27d39f695765e6427e0d0a66d01df1c16e915c3
Gus1101 <augustosantosb60@gmail.com>
2025-05-26 19:44:35 -0300

    build: adicionando contratos de dados

--- models/models_data_handler_oop.py
+++ models/models_data_handler_oop.py
@@ -8,8 +8,6 @@ class SchemaExtractExcelDataOut(pa.DataFrameModel):
     customer_email: Series[str]
     customer_age: Series[int] = pa.Field(ge=1, le=100)
 
-
-
 class SchemaTransformDataIn(SchemaExtractExcelDataOut):
     pass
 
