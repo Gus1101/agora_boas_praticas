a7d52967fcbd3ed954a220f7214f5a82ffb6f7ec
Gus1101 <augustosantosb60@gmail.com>
2025-05-26 18:27:09 -0300

    tests: adicionando teste de confiabildiade para data_handler_oop

---
+++ tests/test_src_backend_data_handler_data_handler_oop.py
@@ -0,0 +1,23 @@
+#Libs Import
+import pytest
+
+from src.backend.data_handler.data_handler_oop import ExcelHandler
+from pathlib import Path
+
+#Tests
+def test_extract_data_excel_path_is_type_path():
+
+    """
+    Função para testar se o path indicado no método extract_data_excel é do tipo Path.
+    """
+
+    #Arrange
+    excel_handler = ExcelHandler()
+    path_read = "string"
+
+    #Act
+    with pytest.raises(TypeError):
+        excel_handler.extract_excel_data(path=path_read)
+
+    #Assert
+    assert "O paramêtro path deve receber um valor de tipo Path"
